9d963bfa511b1cc67f56718774c93872da6fa4d4
Shortturtle <67746558+Shortturtle@users.noreply.github.com>
2025-04-30 14:47:24 +0800

    Miscellaneous Changes

---
+++ Assets/Scripts/PlayerStatsCreator.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Player Data")] //Create a new playerData object by right clicking in the Project Menu then Create/Player/Player Data and drag onto the player
+public class PlayerData : ScriptableObject
+{
+    [Header("Gravity")]
+    [HideInInspector] public float gravityStrength; //Gravity needed for the desired jumpHeight and jumpTimeToApex.
+    [HideInInspector] public float gravityScale; //Strength of the player's gravity as a multiplier of gravity (set in ProjectSettings/Physics2D).
+                                                 //Also the value the player's rigidbody2D.gravityScale is set to.
+    [Space(5)]
+    public float fallGravityMultiplier; //Multiplier to the player's gravityScale when falling.
+    public float maxFallSpeed; //Maximum fall speed of the player when falling.
+    [Space(20)]
+
+    [Header("Run")]
+    public float runMaxSpeed; //Target speed we want the player to reach.
+    public float runAcceleration; //The speed at which our player accelerates to max speed, can be set to runMaxSpeed for instant acceleration down to 0 for none at all
+    [HideInInspector] public float runAccelAmount; //The actual force (multiplied with speedDiff) applied to the player.
+    public float runDecceleration; //The speed at which our player decelerates from their current speed, can be set to runMaxSpeed for instant deceleration down to 0 for none at all
+    [HideInInspector] public float runDeccelAmount; //Actual force (multiplied with speedDiff) applied to the player .
+    [Space(5)]
+    [Range(0f, 1)] public float accelInAir; //Multipliers applied to acceleration rate when airborne.
+    [Range(0f, 1)] public float deccelInAir; //Multipliers applied to decceleration rate when airborne.
+
+    [Space(20)]
+
+    [Header("Jump")]
+    public float jumpHeight; //Height of the player's jump
+    public float jumpTimeToApex; //Time between applying the jump force and reaching the desired jump height. These values also control the player's gravity and jump force.
+    [HideInInspector] public float jumpForce; //The actual force applied (upwards) to the player when they jump.
+
+    [Header("Both Jumps")]
+    public float jumpCutGravityMultiplier; //Multiplier to increase gravity if the player releases the jump button while still jumping
+    [Space(20)]
+
+    [Header("Assists")]
+    [Range(0.01f, 0.5f)] public float coyoteTime; //Grace period after falling off a platform, where you can still jump
+    [Range(0.01f, 0.5f)] public float jumpInputBufferTime; //Grace period after pressing jump where a jump will be automatically performed once the requirements (eg. being grounded) are met.
+
+
+    //Unity Callback, called when the inspector updates
+    private void OnValidate()
+    {
+        //Calculate gravity strength
+        gravityStrength = -(2 * jumpHeight) / (jumpTimeToApex * jumpTimeToApex);
+
+        //Calculate the rigidbody's gravity scale
+        gravityScale = gravityStrength / Physics2D.gravity.y;
+
+        //Calculate are run acceleration & deceleration forces
+        runAccelAmount = ((1 / Time.fixedDeltaTime) * runAcceleration) / runMaxSpeed;
+        runDeccelAmount = ((1 / Time.fixedDeltaTime) * runDecceleration) / runMaxSpeed;
+
+        //Calculate jumpForce
+        jumpForce = Mathf.Abs(gravityStrength) * jumpTimeToApex;
+
+        #region Variable Ranges
+        runAcceleration = Mathf.Clamp(runAcceleration, 0.01f, runMaxSpeed);
+        runDecceleration = Mathf.Clamp(runDecceleration, 0.01f, runMaxSpeed);
+        #endregion
+    }
+}
+
